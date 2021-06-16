2c9d8de98e24d9ea1723c1fbd8c59d224a14622a
Nexi <nexilios@gmail.com>
2021-06-16 15:19:10 +0700

    Go Back Btn

--- app/src/main/java/com/example/findjobnow/ProfileActivity.java
+++ app/src/main/java/com/example/findjobnow/ProfileActivity.java
@@ -1,14 +1,20 @@
 package com.example.findjobnow;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity{
 
     private TextView mTextView;
+
+    Button LoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +23,20 @@ public class ProfileActivity extends AppCompatActivity{
 
         mTextView = (TextView) findViewById(R.id.text);
 
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Load home page
+                Intent intent = new Intent(ProfileActivity.this, HomeActivity.class);
+                ProfileActivity.this.finish();
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
 
     public void openDatePicker(View view) {
     }
